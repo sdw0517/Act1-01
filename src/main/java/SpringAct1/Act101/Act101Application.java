4b2f8422dd08d5c943dc81500eedd4a22c01743e
dongwan-seo <dongwan051700@naver.com>
2023-01-06 13:09:10 +0900

    Initial commit

---
+++ src/main/java/SpringAct1/Act101/Act101Application.java
@@ -0,0 +1,13 @@
+package SpringAct1.Act101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Act101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Act101Application.class, args);
+	}
+
+}
---
+++ src/test/java/SpringAct1/Act101/Act101ApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringAct1.Act101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Act101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
