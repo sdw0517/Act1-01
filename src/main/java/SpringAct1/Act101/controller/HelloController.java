fe57aa1dd9d3f9e0f31cf0beb726ca4256468bf4
dongwan-seo <dongwan051700@naver.com>
2023-01-06 15:07:48 +0900

    23/01/06 View 환경설정

---
+++ src/main/java/SpringAct1/Act101/controller/HelloController.java
@@ -0,0 +1,15 @@
+package SpringAct1.Act101.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello"; //return.html 호출
+    }
+}
